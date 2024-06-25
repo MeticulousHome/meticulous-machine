a306bfd9f47be189fc6ac45ac2e05c129b38983d
Mimoja <mimoja@meticuloushome.com>
2024-06-26 01:33:33 +0200

    config: fixup repository urls and packages
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- config.sh
+++ config.sh
@@ -7,42 +7,42 @@ readonly SERVICES_DIR="system-services"
 readonly RAUC_CONFIG_DIR="rauc-config"
 
 readonly DEBIAN_SRC_DIR=${COMPONENTS_DIR}/"debian"
-readonly DEBIAN_GIT="git@github.com:FFFuego/debian-variscite-fika"
+readonly DEBIAN_GIT="git@github.com:MeticulousHome/debian-variscite-fika"
 readonly DEBIAN_BRANCH="debian-bullseye"
 readonly DEBIAN_REV="HEAD"
 
 readonly BACKEND_SRC_DIR="${COMPONENTS_DIR}/meticulous-backend"
-readonly BACKEND_GIT="git@github.com:FFFuego/backend_for_esp32/"
+readonly BACKEND_GIT="git@github.com:MeticulousHome/backend_for_esp32/"
 readonly BACKEND_BRANCH="mimoja_dev"
 readonly BACKEND_REV="HEAD"
 
 readonly DIAL_SRC_DIR="${COMPONENTS_DIR}/meticulous-dial"
-readonly DIAL_GIT="git@github.com:FFFuego/meticulous-dial"
+readonly DIAL_GIT="git@github.com:MeticulousHome/meticulous-dial"
 readonly DIAL_BRANCH="beta"
 readonly DIAL_REV="HEAD"
 
 readonly DASH_SRC_DIR="${COMPONENTS_DIR}/meticulous-dashboard"
-readonly DASH_GIT="git@github.com:FFFuego/meticulous-frontend"
+readonly DASH_GIT="git@github.com:MeticulousHome/meticulous-frontend"
 readonly DASH_BRANCH="mimoja_wip"
 readonly DASH_REV="HEAD"
 
 readonly WEB_APP_SRC_DIR="${COMPONENTS_DIR}/meticulous-web-app"
-readonly WEB_APP_GIT="git@github.com:FFFuego/meticulous-web-app"
+readonly WEB_APP_GIT="git@github.com:MeticulousHome/meticulous-web-app"
 readonly WEB_APP_BRANCH="main"
 readonly WEB_APP_REV="HEAD"
 
 readonly WATCHER_SRC_DIR="${COMPONENTS_DIR}/meticulous-watcher"
-readonly WATCHER_GIT="git@github.com:FFFuego/meticulous-watcher"
+readonly WATCHER_GIT="git@github.com:MeticulousHome/meticulous-watcher"
 readonly WATCHER_BRANCH="main"
 readonly WATCHER_REV="HEAD"
 
 readonly FIRMWARE_SRC_DIR="${COMPONENTS_DIR}/meticulous-firmware"
 readonly FIRMWARE_OUT_DIR="${COMPONENTS_DIR}/meticulous-firmware-build"
-readonly FIRMWARE_GIT="git@github.com:FFFuego/flow_machine_firmware"
+readonly FIRMWARE_GIT="git@github.com:MeticulousHome/flow_machine_firmware"
 readonly FIRMWARE_BRANCH="dev"
 readonly FIRMWARE_REV="HEAD"
 
-readonly SYSTEM_PACKAGES="avahi-daemon mpv"
+readonly SYSTEM_PACKAGES="avahi-daemon mpv libgirepository1.0-dev"
 readonly DEVELOPMENT_PACKAGES="git rsync bash-completion"
 readonly HOST_PACKAGES="\
     binfmt-support pv qemu-user-static debootstrap kpartx lvm2 dosfstools gpart\
