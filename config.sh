1e5fb9482bc0c4fc78f5b38415aa97654d038a36
oscarlr16 <oscarleon.ro16@gmail.com>
2024-07-19 15:19:45 -0600

    chore: update Debian repo config for PCB tester setup
    - Change DEBIAN_GIT to personal fork
    - Set DEBIAN_BRANCH to 'set_up_pcb_tester'
    
    Changes should remain in 'set_up_pcb_tester' branch only

--- config.sh
+++ config.sh
@@ -10,8 +10,8 @@ readonly MISC_DIR="misc"
 readonly RAUC_CERT="beta.rsa4096.cert.pem"
 
 readonly DEBIAN_SRC_DIR=${COMPONENTS_DIR}/"debian"
-readonly DEBIAN_GIT="git@github.com:MeticulousHome/debian-variscite-fika"
-readonly DEBIAN_BRANCH="debian-bullseye"
+readonly DEBIAN_GIT="git@github.com:oscarlr16/debian-variscite-fika-oscar.git"
+readonly DEBIAN_BRANCH="set_up_pcb_tester"
 readonly DEBIAN_REV="HEAD"
 
 readonly BACKEND_SRC_DIR="${COMPONENTS_DIR}/meticulous-backend"
