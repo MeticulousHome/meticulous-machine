6e5f1b004d0e1035e74b0dec7e0f6d5789a751a4
oscarlr16 <oscarleon.ro16@gmail.com>
2024-07-22 13:12:29 -0600

    refactor(script): install AutomatedMainBoardTesterBackend in /opt
    - Change installation path to /opt for consistency

--- make-rootfs.sh
+++ make-rootfs.sh
@@ -74,7 +74,7 @@ function b_copy_components() {
     
     # Install AutomatedMainBoardTesterBackend
     echo "Installing AutomatedMainBoardTesterBackend"
-    cp -r ${TESTER_BACKEND_SRC_DIR} ${ROOTFS_DIR}/root/AutomatedMainBoardTesterBackend
+    cp -r ${TESTER_BACKEND_SRC_DIR} ${ROOTFS_DIR}/opt/AutomatedMainBoardTesterBackend
 
     if [ -d ${DASH_SRC_DIR}/build ]; then
         # Install Dash
