41fb16ad68dda81810a907058e7bf7c89bb3af46
Mimoja <mimoja@meticuloushome.com>
2024-09-06 16:09:13 -0600

    hawkbit: set target name as well
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- rauc-config/create_config.sh
+++ rauc-config/create_config.sh
@@ -12,5 +12,6 @@ echo "Update Channel is ${update_channel}"
 
 cp  /etc/hawkbit/config.conf.template /etc/hawkbit/config.conf
 
+sed -i "s/__TARGET_NAME__/$(hostname)/" /etc/hawkbit/config.conf
 sed -i "s/__BOOT_MODE__/${boot_mode}/" /etc/hawkbit/config.conf
 sed -i "s/__UPDATE_CHANNEL__/${update_channel}/" /etc/hawkbit/config.conf
