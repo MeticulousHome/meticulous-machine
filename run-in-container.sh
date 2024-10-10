b06af18da0b037bde09902e1ee3bff335d4fb1ad
Mimoja <mimoja@meticuloushome.com>
2024-10-10 16:23:08 +0200

    start adapting the github CI for the new imaage building
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- run-in-container.sh
+++ run-in-container.sh
@@ -5,7 +5,7 @@ IMAGE_NAME="meticulous_docker_host_image"
 
 echo -e "\033[93mBuilding Docker image...\033[0m"
 
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME -f image-builder.Dockerfile .
 
 echo -e "\033[93mRunning container...\033[0m"
 
