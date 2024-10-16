b0d11315b2cca269a88f11429a629ad477b04225
Angel-Padilla <angel.p.lopez@outlook.com>
2024-10-15 18:27:34 -0600

    fix(media_handler): usb mass storage handle for recovery update is working
    The purpose of media_handler.sh now is just to start the usb-rauc-install.service
    and provision it the name of the device that triggered the udev rule, with this
    the udev rule executable does not have to mount and look for files before, as it is
    now done by the service itself. If there is a recovery update going on, the script
    will not try to start the service, so it will just "reject" any new usb drive
    
    This script now is in charge of looking for the rauc bundle and deciding to start a
    recovery update or just notify the backend of the new media. It can look for the bundle
    in any partition of the plugged in device. If by any reason, there is bundles in more
    than one partition, it will install the first bundle found -from the partition with the
    lowest number-
    
    Now it catches the name of the whole device instead of only the partitions, in fact now it
    ignores the partitions, so it is only triggered once for every device despite the its number
    of partitions

--- config/etc/usb_updater/media_handler.sh
+++ config/etc/usb_updater/media_handler.sh
@@ -1,88 +1,37 @@
 #!/bin/bash
 
-MOUNT_POINT="/tmp/possible_updater"
+UPDATING_FLAG_FILE="/tmp/updating"
 DEVICE_KERNEL_NAME="$1"
-PARTITION_NUMBER=$2
 
-USB_PATH="/dev/$DEVICE_KERNEL_NAME"
-
-USB_AS_UPDATER=true
-rauc_files=""
-
-#if the mount point exists, it is being updated, discard this usb as updater device
-if [ -d $MOUNT_POINT ]; then
-    USB_AS_UPDATER=false
-else
-
-    echo "partition number received: $PARTITION_NUMBER"
-
-    if [ "$PARTITION_NUMBER" == "1" ]; then
-        mkdir $MOUNT_POINT
-        echo "attempting to mount: $USB_PATH"
-        mount $USB_PATH $MOUNT_POINT 1>&2
-        error_mounting="$?"
-        if [ $error_mounting == "0" ]; then
-            echo "device mounted"
-        else
-            echo "device $USB_PATH could not be mounted: $error_mounting"
-            rm -r $MOUNT_POINT
-            exit 1
-        fi
-    rauc_files=$(find "$MOUNT_POINT" -maxdepth 1 -type f -name "*.raucb")
-    else
-        USB_AS_UPDATER=false
-    fi
+#if a partition is mounted
+if [ -f $UPDATING_FLAG_FILE ]; then
+    echo "machine is updating"
+    exit 1
 fi
 
-#mounting usb device
-if [ $USB_AS_UPDATER == true ] && [ -n "$rauc_files" ]; then
-    echo "Stopping [ rauc-hawkbit-updater.service ] and starting [ rauc_installer.service ]"
-    systemctl stop rauc-hawkbit-updater.service
-    #export the kernel name of the device to mount it on the rauc_install service
-    #FIXME When doing the update from the rauc-hawkbit-client we might not need to export them, but send a signal directly with them as parameters
+echo "device to analyze: $DEVICE_KERNEL_NAME"
 
-    umount $MOUNT_POINT
-    systemctl start usb-rauc-install.service
+systemctl start usb-rauc-install.service
 
-    #wait for the service to be up, and send the data through a dbus signal
-    sleep 2
-    busctl --system emit /handlers/massStorage com.Meticulous.Handler.MassStorage Updater s "/dev/$DEVICE_KERNEL_NAME"
-    
-    #wait for the service to respond
-    timeout --foreground 2 bash -c '
-    while read -r line ; do
-       echo $line
-       if [ -n "$(echo $line | grep /handlers/Updater)" ]; then
-           echo "usb-rauc-install.service is up"
-           break
-       fi
-    done < <(dbus-monitor --system "interface=com.Meticulous.Handler.Updater, member=Alive")
-    '
+#wait for the service to be up, and send the data through a dbus signal
+sleep 1
+busctl --system emit /handlers/massStorage com.Meticulous.Handler.MassStorage Updater s "$DEVICE_KERNEL_NAME"
 
-    if [ $? -eq 124 ]; then
-        echo "usb-rauc-install.service didnt respond"
-        exit 3
-    else
-        echo "usb-rauc-install.service started"
-        exit 0
+#wait for the service to respond
+timeout --foreground 5 bash -c '
+while read -r line ; do
+    echo $line
+    if [ -n "$(echo $line | grep /handlers/Updater)" ]; then
+        echo "usb-rauc-install.service is up"
+        break
     fi
+done < <(dbus-monitor --system "interface=com.Meticulous.Handler.Updater, member=Alive")
+'
+
+if [ $? -eq 124 ]; then
+    echo "usb-rauc-install.service didnt respond"
+    exit 2
 else
-    if [ $USB_AS_UPDATER == false ]; then
-        echo "updater device already mounted or partition not valid"
-    else
-        if [ -z "$rauc_files" ]; then
-            echo "RAUC file not found in $USB_PATH, cleaning"
-            umount $MOUNT_POINT
-            rm -r $MOUNT_POINT
-        fi
-    fi
-    #check if the variable currently updating exists
-    if [ -z "$DEVICE_KERNEL_NAME" ]; then
-        exit 2
-    fi
-    #emiting dbus signal for a new mass storage device
-    echo "Notifying backend of new media"
-    busctl --system emit /handlers/massStorage com.Meticulous.Handler.MassStorage NewUSB s "/dev/$DEVICE_KERNEL_NAME"
-    
+    echo "usb-rauc-install.service started"
     exit 0
 fi
