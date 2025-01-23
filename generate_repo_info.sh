3d2478266399880750724649106cc1e8dedc955e
Octavio2121 <tavo._22@hotmail.com>
2025-01-22 19:12:07 -0600

    ci/cd: The directories for each component are handled specially to allow access to their repository-info.txt file

--- generate_repo_info.sh
+++ generate_repo_info.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
-# Directory where all the reports will be saved
 REPORT_DIR="components/repo-info"
 mkdir -p $REPORT_DIR
 
-# List of components and their artifact directories
 declare -A components=(
-    ["bootloader"]="components/bootloader/build"
-    ["linux"]="components/linux-build"
+    ["bootloader"]="components/bootloader/uboot"
+    ["linux"]="components/linux"
     ["debian"]="components/debian-base"
-    ["psplash"]="components/psplash-build"
-    ["rauc"]="components/rauc/build"
-    ["dial"]="components/meticulous-dial/out/make/deb/arm64"
-    ["web"]="components/meticulous-web-app/out"
-    ["firmware"]="components/meticulous-firmware-build"
-    ["history"]="components/meticulous-history-ui/build"
-    ["plotter"]="components/meticulous-plotter-ui/build"
+    ["psplash"]="components/psplash"
+    ["rauc"]="components/rauc/rauc"
+    ["dial"]="components/meticulous-dial"
+    ["web"]="components/meticulous-web-app"
+    ["firmware"]="components/meticulous-firmware"
+    ["history"]="components/meticulous-history-ui"
+    ["plotter"]="components/meticulous-plotter-ui"
 )
 
-# Create a summary file
 echo "Repository Information Summary - $(date)" > "$REPORT_DIR/summary.txt"
 echo "----------------------------------------" >> "$REPORT_DIR/summary.txt"
 
-# Gather information from each component
 for component in "${!components[@]}"; do
     echo "## $component ##" >> "$REPORT_DIR/summary.txt"
     
-    # Search for the repository-info.txt in the component directory
     if [ -f "${components[$component]}/repository-info.txt" ]; then
         # Copy the original file
         cp "${components[$component]}/repository-info.txt" "$REPORT_DIR/${component}-info.txt"
@@ -38,7 +33,5 @@ for component in "${!components[@]}"; do
     echo "" >> "$REPORT_DIR/summary.txt"
 done
 
-# Compress all the information files
 tar -czf repo-info.tar.gz -C "$REPORT_DIR" .
-
 echo "Repository information has been compiled and saved in repo-info.tar.gz"
