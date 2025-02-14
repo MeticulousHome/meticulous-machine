2ad2353538df35c4506dc778abe7cd82b43bd97d
Mimoja <git@mimoja.de>
2025-02-14 01:07:05 +0100

    images/stable: move beta to stable
    Signed-off-by: Mimoja <git@mimoja.de>

---
+++ images/stable.versions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export LINUX_GIT="git@github.com:MeticulousHome/linux-variscite-fika.git"
+export LINUX_BRANCH="debian-6.1.36"
+export LINUX_REV="c5db7fb607994bb8e93c7fcd4b2bfb5597583085" # video: update boot logo to meticulous DOT
+export UBOOT_REV="7fdc6b1de1358248e7743069ff335f4168228374" # Enable fastboot recovery on boot via recovery pin
+export ATF_REV="7575633e03ff952a18c0a2c0aa543dee793fda5f" # imx: Fix multiple definition of ipc_handle
+export IMX_MKIMAGE_REV="6745ccdcf15384891639b7ced3aa6ce938682365" # imx8m: Add script to generate SIT (secondary image table)
+export DEBIAN_BRANCH="bookworm"
+export DEBIAN_REV="2cca02c2a06e3213e23727ca7cd323676001ca57" # weston: build vnc and rdp support
+export BACKEND_REV="5cdcf558f7e3a746bb7bd01d4b79212f20452eea" # fix: user timezone overwritten
+export DIAL_REV="c97759abe3c42ab3b464f0484663d1eeb613bed1" # chore(release): v1.35.1 [skip ci]
+export WEB_APP_REV="52a271aefd496b5decf149eb7931d872f77d1cba" # chore(deps): bump the npm_and_yarn group across 1 directory with 9 updates
+export WATCHER_REV="2a8c95768707594c86100fb4bf9fc2527865d077" # Merge pull request #6 from Octavio2121/notify
+export FIRMWARE_REV="feabd9b503b56065b9878cafc8ff9619f88a867a" # fix: change dynamic creation of ACAIA's update task to static creation
+export RAUC_REV="269e1721b3c5f6512f7dfb7fd19f9108515c2a98" # debian: Disable test building for now
+export HAWKBIT_REV="dcd2e794d959ae916e39820547babeb597216fab" # Comment translated
+export PSPLASH_REV="5b0085b559f1aac09c0a88f85f203ab332bbe1c9" # Replace baseimage with DOT image
+export HISTORY_UI_REV="8045d4cf84481d9586ddc8dd3bb94d5cee29a959" # Modified ranges in each pressure test
+export PLOTTER_UI_REV="ad394f46889317926ef319df50f7ffe4a790dc99" # Merge pull request #6 from MeticulousHome/feat/sort-and-zoom
