464c0bebc9c7fa5e7738e17eadbd019711204184
Mimoja <git@mimoja.de>
2025-02-14 15:20:22 +0100

    hawkbit: send serial if met-config tool is available
    Signed-off-by: Mimoja <git@mimoja.de>

--- config/etc/hawkbit/create_config.sh
+++ config/etc/hawkbit/create_config.sh
@@ -12,6 +12,13 @@ echo "Update Channel is ${update_channel}"
 
 cp  /etc/hawkbit/config.conf.template /etc/hawkbit/config.conf
 
+serial="UNSET"
+
+if [ ! -z "$(which met-config)" ]; then
+  serial=$(met-config get .system.serial)
+fi
+
 sed -i "s/__TARGET_NAME__/$(hostname)/" /etc/hawkbit/config.conf
 sed -i "s/__BOOT_MODE__/${boot_mode}/" /etc/hawkbit/config.conf
 sed -i "s/__UPDATE_CHANNEL__/${update_channel}/" /etc/hawkbit/config.conf
+sed -i "s/__SERIAL__/${serial}/" /etc/hawkbit/config.conf
