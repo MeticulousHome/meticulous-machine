5c0f4873c61723055ac7eddce7ede026cee421d7
Angel-Padilla <angel.p.lopez@outlook.com>
2025-03-03 14:23:24 -0600

    update rauc config file: add boot_partition and build_date to device attributes

--- config/etc/hawkbit/create_config.sh
+++ config/etc/hawkbit/create_config.sh
@@ -2,8 +2,10 @@
 
 if grep -q "root=/dev/mmcblk1" /proc/cmdline; then
   export boot_mode="sdcard";
+  boot_partition="sdcard"  #get boot partition
 else
   export boot_mode="emmc";
+  boot_partition=$(rauc status --output-format=json-pretty | grep -oP '"booted" : "\K[^"]*' || echo UNKNOWN)  #get boot partition
 fi;
 
 echo "Boot mode is ${boot_mode}"
@@ -18,7 +20,12 @@ if [ ! -z "$(which met-config)" ]; then
   serial=$(met-config get .system.serial)
 fi
 
+build_date=$(cat /opt/ROOTFS_BUILD_DATE || echo UNKNOWN)                                          #get booted image build date
+
+
 sed -i "s/__TARGET_NAME__/$(hostname)/" /etc/hawkbit/config.conf
 sed -i "s/__BOOT_MODE__/${boot_mode}/" /etc/hawkbit/config.conf
 sed -i "s/__UPDATE_CHANNEL__/${update_channel}/" /etc/hawkbit/config.conf
 sed -i "s/__SERIAL__/${serial}/" /etc/hawkbit/config.conf
+sed -i "s/__BOOTED__/${boot_partition}/" /etc/hawkbit/config.conf
+sed -i "s/__BUILD_DATE__/${build_date}/" /etc/hawkbit/config.conf
