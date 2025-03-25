10e2a0b7c8ca26b5b12f753cb4042e9b88d05db2
AUOR17 <angel_osorno@outlook.com>
2025-03-25 17:45:03 -0600

    Added compatibility version check

--- images/rel_motor.versions.sh
+++ images/rel_motor.versions.sh
@@ -4,3 +4,4 @@ export DEBIAN_BRANCH="bookworm-mainline"
 export DEBIAN_REV="HEAD"
 export LINUX_BRANCH="linux-6.12.y"
 export LINUX_REV="HEAD"
+export FIRMWARE_REV="b010a226fe9fd7ec5e2d78e65f883d570715a34f" # Added constants for external thermistors
