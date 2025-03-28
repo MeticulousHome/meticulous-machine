dc36baab8b674d4d522935af0af4a36f28695da2
AUOR17 <angel_osorno@outlook.com>
2025-03-28 12:13:01 -0600

    Added memory leak commit for reproduce OOM bug in the dial

--- images/rel_motor.versions.sh
+++ images/rel_motor.versions.sh
@@ -5,3 +5,4 @@ export DEBIAN_REV="HEAD"
 export LINUX_BRANCH="linux-6.12.y"
 export LINUX_REV="HEAD"
 export FIRMWARE_REV="b010a226fe9fd7ec5e2d78e65f883d570715a34f" # Added constants for external thermistors
+export DIAL_REV="57c1ec775983b3b3213991378d106aaadaf3b9f8" # test changing screens to replicate the OOM error 
