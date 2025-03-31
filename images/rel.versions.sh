003e11abbcaa74f4d7795caf598a4eef6e3237dd
AUOR17 <angel_osorno@outlook.com>
2025-03-31 13:55:10 -0600

    Added Backend commit working with rel software

--- images/rel.versions.sh
+++ images/rel.versions.sh
@@ -4,4 +4,5 @@ export DEBIAN_BRANCH="bookworm-mainline"
 export DEBIAN_REV="HEAD"
 export LINUX_BRANCH="linux-6.12.y"
 export LINUX_REV="HEAD"
-export DIAL_REV="57c1ec775983b3b3213991378d106aaadaf3b9f8" # test changing screens to replicate the OOM error
+# export DIAL_REV="57c1ec775983b3b3213991378d106aaadaf3b9f8" # test changing screens to replicate the OOM error
+export BACKEND_REV="bd9d83fef4213a407a203b3a68cc397504f52685" # backend working
