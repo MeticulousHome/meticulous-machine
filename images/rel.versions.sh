e7e675136ad8f6fb0f7390c1b7abb2a23eb66c02
AUOR17 <angel_osorno@outlook.com>
2025-04-01 11:39:16 -0600

    Deactivate sentry for rel purpouses

--- images/rel.versions.sh
+++ images/rel.versions.sh
@@ -5,5 +5,5 @@ export DEBIAN_REV="HEAD"
 export LINUX_BRANCH="linux-6.12.y"
 export LINUX_REV="HEAD"
 # export DIAL_REV="57c1ec775983b3b3213991378d106aaadaf3b9f8" # test changing screens to replicate the OOM error
-export BACKEND_REV="bd9d83fef4213a407a203b3a68cc397504f52685" # backend working
+export BACKEND_REV="579afc06741e248bd0042fac79a04cf221ed0b62" # backend working
 export FIRMWARE_REV="ba8a4ac6d729fd034382099f848873cd3d28d199" # Added constants for external thermistors
