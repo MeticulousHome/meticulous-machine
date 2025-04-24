f73dbb514f83611a85fe5ad24a21cb4129d0a2e8
AUOR17 <angel_osorno@outlook.com>
2025-04-23 19:07:11 -0600

    feat(rel.versions.sh): Test new approach related to OOM error Added branch to test if the OOM error realtionated with the dial app is fixed

--- images/rel.versions.sh
+++ images/rel.versions.sh
@@ -7,4 +7,4 @@ export LINUX_REV="HEAD"
 # export DIAL_REV="57c1ec775983b3b3213991378d106aaadaf3b9f8" # test changing screens to replicate the OOM error
 export BACKEND_REV="579afc06741e248bd0042fac79a04cf221ed0b62" # backend working
 export FIRMWARE_REV="ba8a4ac6d729fd034382099f848873cd3d28d199" # Added constants for external thermistors
-export DIAL_REV="02d9c08a8f0f9d48251454fa4e68bd8d501a525d"
+export DIAL_REV="62f88949139c9c4de6cf6bd91a8227ca2d00f72a" 
