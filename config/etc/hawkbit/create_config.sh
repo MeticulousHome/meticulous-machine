2498b28e1e81d9ac786146d87937f21b3cdc9559
Mimoja <git@mimoja.de>
2025-05-10 21:11:51 +0200

    hawkbit: report the booted images channel as well as the machines set channel
    Signed-off-by: Mimoja <git@mimoja.de>

--- config/etc/hawkbit/create_config.sh
+++ config/etc/hawkbit/create_config.sh
@@ -21,7 +21,7 @@ if [ ! -z "$(which met-config)" ]; then
 fi
 
 build_date=$(cat /opt/ROOTFS_BUILD_DATE || echo UNKNOWN)                                          #get booted image build date
-
+build_channel=$(cat /opt/image-build-channel | echo UNKNOWN)
 
 sed -i "s/__TARGET_NAME__/$(hostname)/" /etc/hawkbit/config.conf
 sed -i "s/__BOOT_MODE__/${boot_mode}/" /etc/hawkbit/config.conf
@@ -29,3 +29,4 @@ sed -i "s/__UPDATE_CHANNEL__/${update_channel}/" /etc/hawkbit/config.conf
 sed -i "s/__SERIAL__/${serial}/" /etc/hawkbit/config.conf
 sed -i "s/__BOOTED__/${boot_partition}/" /etc/hawkbit/config.conf
 sed -i "s/__BUILD_DATE__/${build_date}/" /etc/hawkbit/config.conf
+sed -i "s/__BUILD_CHANNEL__/${build_date}/" /etc/hawkbit/config.conf
