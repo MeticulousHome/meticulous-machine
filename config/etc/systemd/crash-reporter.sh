27d0927a59edba506f823d7a43e0bb8eeb94211d
Angel-Padilla <angel.p.lopez@outlook.com>
2025-05-23 17:33:58 -0600

    chore: move service restart logic
    When a service fails, it does not immediately calls the onFailure service
    dependency, as it needs to enter the fail state and stay there, when the
    service has the Restart= config set to restart the service when it fails,
    the service does not stay on the failed state but immediatly goes to restarting
    skipping the call to onFailure. The only way to trigger onFAilure in this case is
    to deplete the number of allowed restarts, and only then, onFailure is triggered
    
    This commit removes the ability to restart the services that have a onFailure
    dependency to restart the service when it fails, allowing the onFailure trigger
    every time it fails, and moves the logic to the crash-reporter@.service script
    that decides which services are expected to be restarted when they fail

---
+++ config/etc/systemd/crash-reporter.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+# get a journal entry with the service that failed
+echo "$MONITOR_UNIT has failed - restarting"
+
+# report the error to sentry
+/opt/meticulous-crash-reporter
+
+#restart the service if meant to be restarted
+
+declare -a restart
+restart=(
+    "meticulous-backend.service"
+    "meticulous-dial.service"
+    "meticulous-watcher.service"
+    "rauc-hawkbit-updater.service"
+)
+
+for SERVICE in "${restart[@]}"; do
+
+    [ "$SERVICE" = "$MONITOR_UNIT" ] && systemctl restart $MONITOR_UNIT && break
+
+done
