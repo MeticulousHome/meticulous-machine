2627f2fe5fc864166c917e0faa9549a7c0b7600d
Mimoja <git@mimoja.de>
2025-07-03 18:18:40 +0200

    generate-repo-info: use paths from config.sh
    Signed-off-by: Mimoja <git@mimoja.de>

--- generate_repo_info.sh
+++ generate_repo_info.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source config.sh
 REPORT_DIR="components/repo-info"
 mkdir -p $REPORT_DIR
 
@@ -29,20 +30,20 @@ function generate_repo_info() {
 }
 
 declare -A components=(
-    ["bootloader"]="components/bootloader/build/repo-info/repository-info.txt"
-    ["linux"]="components/linux-build/repo-info/repository-info.txt"
-    ["debian"]="components/debian-base/repo-info/repository-info.txt"
-    ["psplash"]="components/psplash-build/repo-info/repository-info.txt"
-    ["rauc"]="components/rauc/build/repo-info/repository-info.txt"
-    ["dial"]="components/meticulous-dial/out/make/deb/arm64/repo-info/repository-info.txt"
-    ["web"]="components/meticulous-web-app/out/repo-info/repository-info.txt"
-    ["firmware"]="components/meticulous-firmware-build/repo-info/repository-info.txt"
-    ["plotter"]="components/meticulous-plotter-ui/build/repo-info/repository-info.txt"
+    ["bootloader"]="${BOOTLOADER_BUILD_DIR}/repo-info/repository-info.txt"
+    ["linux"]="${LINUX_BUILD_DIR}/repo-info/repository-info.txt"
+    ["debian"]="${DEBIAN_SRC_DIR}/repo-info/repository-info.txt"
+    ["psplash"]="${PSPLASH_BUILD_DIR}/repo-info/repository-info.txt"
+    ["rauc"]="${RAUC_BUILD_DIR}/repo-info/repository-info.txt"
+    ["dial"]="${DIAL_SRC_DIR}/out/make/deb/arm64/repo-info/repository-info.txt"
+    ["web"]="${WEB_APP_SRC_DIR}/out/repo-info/repository-info.txt"
+    ["firmware"]="${FIRMWARE_OUT_DIR}/repo-info/repository-info.txt"
+    ["plotter"]="${PLOTTER_UI_SRC_DIR}/build/repo-info/repository-info.txt"
 )
 
 declare -A special_components=(
-    ["backend"]="components/meticulous-backend"
-    ["watcher"]="components/meticulous-watcher"
+    ["backend"]="${BACKEND_SRC_DIR}"
+    ["watcher"]="${WATCHER_SRC_DIR}"
 )
 
 echo "Repository Information Summary - $(date)" > "$REPORT_DIR/summary.txt"
