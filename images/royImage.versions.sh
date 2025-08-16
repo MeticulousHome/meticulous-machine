baa3f880980f07c60bf9ee78caf6cab7cc8760b1
Angel-Padilla <angel.p.lopez@outlook.com>
2025-08-15 18:03:50 -0600

    add custom image creation for Roy

---
+++ images/royImage.versions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export BACKEND_REV="2a802ddb1595840310d84f47a19cba45cb163962" # increase retraction complete waiting time
+export DIAL_REV="8cdce49f3b246ef7b5e43b9ad6f183b1a4bae303" # fix(preheatscreen): preheat value shown
+export FIRMWARE_REV="4162f67ad385aa07dc57275d4ac6855554db922b" # send bar down temp to the backend
