7eba480180dc98f05004d2b456769ee07dda8be0
Mimoja <git@mimoja.de>
2025-12-01 15:09:39 +0100

    hawkbit: report boot revisions
    Signed-off-by: Mimoja <git@mimoja.de>

--- config/etc/hawkbit/create_config.sh
+++ config/etc/hawkbit/create_config.sh
@@ -17,6 +17,43 @@ get_somrev() {
         echo "$major.$minor"
 }
 
+get_uboot_rev() {
+  device=$1
+
+  # Sanity checks, we dont want the updater to fail to start
+  if [ -z $device ]; then echo "DEV_EMPTY"; return 1; fi
+
+  if [ ! -e $device ]; then echo "ENODEV"; return; fi
+
+  rev=$(busybox strings $device | grep "U-Boot" -m1)
+  if [ -z "$rev" ]; then
+    echo "NULL"
+  else
+    echo "$rev"
+  fi
+}
+
+get_mmc_boot_config() {
+  if [ ! $(which mmc) ] ; then
+    echo "mmc-tool-missing"
+    return 1
+  fi
+
+  raw_part_config=$(mmc extcsd read /dev/mmcblk2 | grep --only "\[PARTITION_CONFIG: 0x..\]" | grep --only "0x..")
+  # part_access=$((${raw_part_config} & 0x3))
+
+  part_boot_active=$(((${raw_part_config} >> 3) & 0x7))
+  if [ ${part_boot_active} -eq 0 ] ; then
+    echo "p1"
+  elif [ ${part_boot_active} -eq 1 ] ; then
+    echo "boot0"
+  elif [ ${part_boot_active} -eq 2 ] ; then
+    echo "boot1"
+  else
+    echo "unknown-${part_boot_active}"
+    return 1
+  fi
+}
 
 if grep -q "root=/dev/mmcblk1" /proc/cmdline; then
   export boot_mode="sdcard";
@@ -44,6 +81,18 @@ build_channel=$(cat /opt/image-build-channel || echo UNKNOWN)
 memory=$(cat /proc/meminfo | grep MemTotal | grep "[0-9]* [a-zA-Z]B" -o)
 som=$(get_somrev)
 
+uboot_disk_rev=$(get_uboot_rev /dev/mmcblk2p1)
+uboot_boot0_rev=$(get_uboot_rev /dev/mmcblk2boot0)
+uboot_boot1_rev=$(get_uboot_rev /dev/mmcblk2boot1)
+uboot_active="/dev/mmcblk2$(get_mmc_boot_config)"
+if [ $? -ne 0 ]; then
+  uboot_active_ref="ERROR"
+else
+  uboot_active_ref=$(get_uboot_rev ${uboot_active})
+fi
+echo "U-Boot booted from ${uboot_active} with version ${uboot_active_ref}"
+
+
 sed -i "s/__TARGET_NAME__/$(hostname)/" /etc/hawkbit/config.conf
 sed -i "s/__BOOT_MODE__/${boot_mode}/" /etc/hawkbit/config.conf
 sed -i "s/__UPDATE_CHANNEL__/${update_channel}/" /etc/hawkbit/config.conf
@@ -53,3 +102,9 @@ sed -i "s/__BUILD_DATE__/${build_date}/" /etc/hawkbit/config.conf
 sed -i "s/__BUILD_CHANNEL__/${build_channel}/" /etc/hawkbit/config.conf
 sed -i "s/__SOM__/${som}/" /etc/hawkbit/config.conf
 sed -i "s/__MEMORY__/${memory}/" /etc/hawkbit/config.conf
+
+sed -i "s/__UBOOT_DISK_REV__/${uboot_disk_rev}/" /etc/hawkbit/config.conf
+sed -i "s/__UBOOT_BOOT0_REV__/${uboot_boot0_rev}/" /etc/hawkbit/config.conf
+sed -i "s/__UBOOT_BOOT1_REV__/${uboot_boot1_rev}/" /etc/hawkbit/config.conf
+sed -i "s|__UBOOT_ACTIVE__|${uboot_active}|" /etc/hawkbit/config.conf
+sed -i "s/__UBOOT_ACTIVE_REV__/${uboot_active_ref}/" /etc/hawkbit/config.conf
