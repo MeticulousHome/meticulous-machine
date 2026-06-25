616d59afee4b3110815b5fed10bfa0cb2b774894
AluUriel <alu.uriel.gp@gmail.com>
2026-06-25 17:06:03 +0800

    fix(lab): include killall for rf test tool

--- config.sh
+++ config.sh
@@ -155,7 +155,7 @@ readonly MOBILE_GIT="git@github.com:MeticulousHome/meticulous-mobile"
 export   MOBILE_BRANCH="nightly"
 export   MOBILE_REV="HEAD"
 
-readonly SYSTEM_PACKAGES="git parted avahi-daemon gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 zstd nginx ssl-cert exfatprogs dnsmasq iptables-persistent gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-alsa gstreamer1.0-plugins-base-apps alembic systemd-timesyncd wireless-regdb iw fonts-noto-color-emoji e2fsprogs mmc-utils curl gpg iputils-ping"
+readonly SYSTEM_PACKAGES="git parted avahi-daemon gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 zstd nginx ssl-cert exfatprogs dnsmasq iptables-persistent gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-alsa gstreamer1.0-plugins-base-apps alembic systemd-timesyncd wireless-regdb iw fonts-noto-color-emoji e2fsprogs mmc-utils curl gpg iputils-ping psmisc"
 readonly DEVELOPMENT_PACKAGES="rsync bash-completion"
 readonly HOST_PACKAGES="\
     binfmt-support pv qemu-user-static debootstrap kpartx lvm2 dosfstools gpart\
