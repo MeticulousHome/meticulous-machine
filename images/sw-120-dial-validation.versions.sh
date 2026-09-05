fa6035de32dd7f2a0f6d53ed709b9fdc733193d9
AluUriel <alu.uriel.gp@gmail.com>
2026-09-05 15:33:34 -0600

    chore(image): build SW-120 from beta head

--- images/sw-120-dial-validation.versions.sh
+++ images/sw-120-dial-validation.versions.sh
@@ -2,4 +2,4 @@
 
 # Isolated Dial package build for SW-120 validation.
 export DIAL_BRANCH="codex/sw-120-long-press-start"
-export DIAL_REV="3446ad4e20e80546200ce91f599b14e8914b7b74"
+export DIAL_REV="82d817c741c7e3ff4878a0feabcab6d83c55115d"
